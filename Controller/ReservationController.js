71af36c8bb61c2c66fec58770b2b85f3c89b27ae
MayyarAA <mayyar1235@gmail.com>
2022-03-04 14:29:11 -0500

    sv able to set status for resrv

--- Controller/ReservationController.js
+++ Controller/ReservationController.js
@@ -3,13 +3,31 @@ import Reservation from '../Model/ReservationModel.js';
 
 const router = express.Router();
 
-router.route('/').post(async (req, res) => {
+router.route('/add/reservation/request').post(async (req, res) => {
 	console.log('req.body ' + req.body);
-	const { CustomerUserName, StoreName, DateTime, Status, Note } = req.body;
+	const {
+		CustomerUserName,
+		StoreName,
+		ServiceProviderId,
+		ServiceType,
+		StartYear,
+		StartMonth,
+		StartDay,
+		StartDateTime,
+		EndDateTime,
+		Status,
+		Note,
+	} = req.body;
 	const newReservation = new Reservation({
 		CustomerUserName,
 		StoreName,
-		DateTime,
+		ServiceProviderId,
+		ServiceType,
+		StartYear,
+		StartMonth,
+		StartDay,
+		StartDateTime,
+		EndDateTime,
 		Status,
 		Note,
 	});
@@ -22,9 +40,9 @@ router.route('/').post(async (req, res) => {
 	}
 });
 
-router.route('/:reservationid').get(async (req, res) => {
-	const reservationidValue = req.params.reservationid;
-	const queryResult = await GetReservationService(reservationidValue);
+router.route('/get/serviceprovider/:userId').get(async (req, res) => {
+	const userId = req.params.userId;
+	const queryResult = await GetReservationSVService(userId);
 	console.log('queryResult ' + JSON.stringify(queryResult));
 	if (queryResult === undefined || queryResult === null) {
 		res.status(400).json(`Error ${error}`);
@@ -35,9 +53,89 @@ router.route('/:reservationid').get(async (req, res) => {
 	return;
 });
 
-const GetReservationService = async (reservationid) => {
-	const result = await Reservation.find({ _id: reservationid })
+router.route('/get/customer/:userId').get(async (req, res) => {
+	const userId = req.params.userId;
+	const queryResult = await GetReservationCustomerService(userId);
+	console.log('queryResult ' + JSON.stringify(queryResult));
+	if (queryResult === undefined || queryResult === null) {
+		res.status(400).json(`Error ${error}`);
+		return;
+	}
+
+	res.json(queryResult);
+	return;
+});
+
+router.route('/patch/serviceprovider/').patch(async (req, res) => {
+	updateReservationStatusService(req, res);
+});
+
+const updateReservationStatusService = async (req, res) => {
+	const {
+		ServiceProviderId,
+		CustomerUserName,
+		StartYear,
+		StartMonth,
+		StartDay,
+		StartDateTime,
+		Status,
+	} = req.body;
+	console.log('ServiceProviderId => ' + ServiceProviderId + ' Status=> ' + Status);
+	const filter = {
+		ServiceProviderId: ServiceProviderId,
+		CustomerUserName: CustomerUserName,
+		StartYear: StartYear,
+		StartMonth: StartMonth,
+		StartDay: StartDay,
+		StartDateTime: StartDateTime,
+	};
+	const updateRequest = { Status: Status };
+	Reservation.findOneAndUpdate(filter, updateRequest, { new: true }, (error, result) => {
+		console.log(error);
+		if (error !== null) {
+			console.log(result);
+			res.status(404).send('error');
+			return;
+		}
+
+		res.status(201).json(result);
+		return;
+	});
+};
+
+const GetReservationCustomerService = async (userId) => {
+	const result = await Reservation.find({ CustomerUserName: userId })
 		.then((resultdata) => {
+			if (
+				resultdata == null ||
+				resultdata == undefined ||
+				Object.keys(resultdata).length === 0 ||
+				resultdata.length == 0
+			) {
+				res.status(404).json('No record found');
+				return;
+			}
+			console.log('resultdata ' + JSON.stringify(resultdata));
+			return resultdata;
+		})
+		.catch((error) => {
+			return res.status(400).json(`Error ${error}`);
+		});
+	return result;
+};
+
+const GetReservationSVService = async (userId) => {
+	const result = await Reservation.find({ ServiceProviderId: userId })
+		.then((resultdata) => {
+			if (
+				resultdata == null ||
+				resultdata == undefined ||
+				Object.keys(resultdata).length === 0 ||
+				resultdata.length == 0
+			) {
+				res.status(404).json('No record found');
+				return;
+			}
 			console.log('resultdata ' + JSON.stringify(resultdata));
 			return resultdata;
 		})
--- Model/ReservationModel.js
+++ Model/ReservationModel.js
@@ -6,13 +6,30 @@ const reservationSchema = new Schema(
 	{
 		CustomerUserName: { type: String, required: true },
 		StoreName: { type: String, required: true },
-		DateTime: { type: Date, required: true },
-		Status: { type: String, required: true },
+		ServiceProviderId: { type: String, required: true },
+		ServiceType: { type: String, required: true },
+		StartYear: { type: Number, required: true },
+		StartMonth: { type: Number, required: true },
+		StartDay: { type: Number, required: true },
+		StartDateTime: { type: Number, required: true },
+		EndDateTime: { type: Number, required: true },
+		//-1:denied,0:pending,1:confired
+		Status: { type: Number, required: true },
 		Note: { type: String, required: false },
 	},
 	{ collection: 'Reservation' }
 );
-reservationSchema.index({ CustomerUserName: 1, StoreName: 1 }, { unique: true });
+reservationSchema.index(
+	{
+		CustomerUserName: 1,
+		ServiceProviderId: 1,
+		StartYear: 1,
+		StartMonth: 1,
+		StartDay: 1,
+		StartDateTime: 1,
+	},
+	{ unique: true }
+);
 const Reservation = mongoose.model('Reservation', reservationSchema);
 
 export default Reservation;
