8b215d1cb49a506b12dc74a681bc8d37c4ed8318
MayyarAA <mayyar1235@gmail.com>
2022-03-04 13:04:23 -0500

    working get spav

--- Controller/ServiceProviderAvailabilityController.js
+++ Controller/ServiceProviderAvailabilityController.js
@@ -2,8 +2,35 @@ import express from 'express';
 import { ServiceProviderAvailability } from '../Model/ServiceProviderAvailability.js';
 
 const router = express.Router();
-router.route('/:date').get(async (req, res) => {
-	const dateRequested = req.params.date;
+router.route('/get/:spid').get(async (req, res) => {
+	const svId = req.params.spid;
+	const yearRequested = req.body.StartYear;
+	const monthRequested = req.body.StartMonth;
+	const dayRequested = req.body.StartDay;
+
+	await ServiceProviderAvailability.find({
+		ServcieProviderId: svId,
+		StartYear: yearRequested,
+		StartMonth: monthRequested,
+		StartDay: dayRequested,
+	})
+		.then((userData) => {
+			if (
+				userData == null ||
+				userData == undefined ||
+				Object.keys(userData).length === 0 ||
+				userData.length == 0
+			) {
+				res.status(404).json('No record found');
+				return;
+			}
+			res.status(200).json(userData);
+			return;
+		})
+		.catch((error) => {
+			res.status(400).json(error);
+			return;
+		});
 });
 
 router.route('/add').post(async (req, res) => {
